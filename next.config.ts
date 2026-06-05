7424027a45174137542c9bd1f49b40ebc35ecc19
KevatAshvin <Kevatashvin@gmail.com>
2026-06-05 08:26:03 +0530

    fix: CSP allow GA/GTM, fix all non-www schema URLs

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,13 @@ const cspDirectives = [
   "default-src 'self'",
   `script-src 'self' 'unsafe-inline' https://www.googletagmanager.com${isDev ? " 'unsafe-eval'" : ""}`,
   "style-src 'self' 'unsafe-inline'",
-  "img-src 'self' blob: data:",
-  "frame-src 'none'",
+  "img-src 'self' blob: data: https://www.google-analytics.com",
   "font-src 'self' data:",
   "connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://www.googletagmanager.com",
   "object-src 'none'",
   "base-uri 'self'",
   "form-action 'self'",
-  "frame-ancestors 'self'",
+  "frame-ancestors 'none'",
   "upgrade-insecure-requests",
 ];
 
